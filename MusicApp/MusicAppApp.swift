ff97e6a8082496a3f6fb2d7cce8d40475a9cf356
Pradeep <rathorep.qualwebs@gmail.com>
2024-02-12 12:06:11 +0530

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Qualwebs on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
